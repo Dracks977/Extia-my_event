e92b81b22f342f4c655d0902e7552b1cba3b5f0c
root <root@localhost.localdomain>
2015-12-08 10:30:50 -0500

    ajout js

--- Experiment/Login/js/ajax.js
+++ Experiment/Login/js/ajax.js
@@ -1,24 +1,24 @@
 $(document).ready( function () { 
-    $("#connexionForm").submit( function() {    // à la soumission du formulaire                         
-        $.ajax({ // fonction permettant de faire de l'ajax
-           type: "POST", // methode de transmission des données au fichier php
-           url: "php/Vlogin.php", // url du fichier php
-           data: "mail="+$("#mail").val()+"&pass="+$("#pass").val(), // données à transmettre
-           success: function(msg){ // si l'appel a bien fonctionné
-                if(msg == "42") // si la connexion en php a fonctionnée
+    $("#connexionForm").submit( function() {                        
+        $.ajax({ 
+           type: "POST", 
+           url: "php/Vlogin.php", 
+           data: "mail="+$("#mail").val()+"&pass="+$("#pass").val(), 
+           success: function(msg){ 
+                if(msg == "42") 
                 {
                     $("span#erreur").html("Vous etes connecter.");
-                    // on désactive l'affichage du formulaire et on affiche un message de bienvenue à la place
+     
                 }
-                else if (msg == "24")  // si la connexion en php n'a pas fonctionnée
+                else if (msg == "24")  
                 {
                     $("span#erreur").html("Erreur lors de la connexion a la base de donnée.");
-                    // on affiche un message d'erreur dans le span prévu à cet effet
+                    
                 }
-                else if (msg == "200")  // si la connexion en php n'a pas fonctionnée
+                else if (msg == "200")  
                 {
                     $("span#erreur").html("Veuillez entrer un Mail.");
-                    // on affiche un message d'erreur dans le span prévu à cet effet
+                    
                 }
                 else if (msg =="201"){
                     $("span#erreur").html("Veuillez entrer un mot de passe.");
@@ -30,6 +30,5 @@ $(document).ready( function () {
                             
            }
         });
-        return false; // permet de rester sur la même page à la soumission du formulaire
-    });
+        return false; 
 });
