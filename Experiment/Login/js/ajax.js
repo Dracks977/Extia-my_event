71cf62502c5647e06f1d743f188c4785d60257dc
root <root@localhost.localdomain>
2015-12-08 10:06:51 -0500

    experiment_Login

---
+++ Experiment/Login/js/ajax.js
@@ -0,0 +1,35 @@
+$(document).ready( function () { 
+    $("#connexionForm").submit( function() {    // à la soumission du formulaire                         
+        $.ajax({ // fonction permettant de faire de l'ajax
+           type: "POST", // methode de transmission des données au fichier php
+           url: "Vlogin.php", // url du fichier php
+           data: "mail="+$("#mail").val()+"&pass="+$("#pass").val(), // données à transmettre
+           success: function(msg){ // si l'appel a bien fonctionné
+                if(msg == "42") // si la connexion en php a fonctionnée
+                {
+                    $("span#erreur").html("Vous etes connecter.");
+                    // on désactive l'affichage du formulaire et on affiche un message de bienvenue à la place
+                }
+                else if (msg == "24")  // si la connexion en php n'a pas fonctionnée
+                {
+                    $("span#erreur").html("Erreur lors de la connexion a la base de donnée.");
+                    // on affiche un message d'erreur dans le span prévu à cet effet
+                }
+                else if (msg == "200")  // si la connexion en php n'a pas fonctionnée
+                {
+                    $("span#erreur").html("Veuillez entrer un Mail.");
+                    // on affiche un message d'erreur dans le span prévu à cet effet
+                }
+                else if (msg =="201"){
+                    $("span#erreur").html("Veuillez entrer un mot de passe.");
+                }
+                 else if (msg =="202"){
+                    $("span#erreur").html("Mauvaise combinaison.");
+                }
+
+                            
+           }
+        });
+        return false; // permet de rester sur la même page à la soumission du formulaire
+    });
+});
---
+++ Experiment/script1_vanhem_n.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE Extia_Event CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE Extia_Event;
+
+CREATE TABLE User
+(
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Nom CHAR(32),
+    Prenom CHAR(32),
+    Mail varchar(255),
+    Pass varchar(1000),
+    Date_creation DATE,
+    Date_modification DATE
+);
+
+CREATE TABLE Event
+(
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Libelle VARCHAR(255),
+    Description VARCHAR(510),
+    image varchar(510),
+    Prix FLOAT,
+    Place INT,
+    Darte DATE,
+    Date_creation DATE,
+    Region INT,
+    Date_modification DATE
+);
+
+CREATE TABLE Categories
+(
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Libelle VARCHAR(255)
+);
+
+CREATE TABLE Region
+(
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Date_creation DATE,
+    Libelle VARCHAR(255)
+);
+
+CREATE TABLE Categorie_Event
+(
+    ID_categorie TINYINT,
+    ID_Event INT
+);
