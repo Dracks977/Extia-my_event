246f69f65eb7dbc4270ec6847bf6e432826d7d2d
Loris THIERRY <thierr_l@g02-r4p04.local>
2015-12-11 17:42:54 +0100

    evenements

---
+++ Experiment/evenements/footer_fleche.js
@@ -0,0 +1,11 @@
+var fc = document.getElementById("flecheorange");
+function fleche() {
+    document.querySelector(".bloc_footer").addEventListener("mouseenter", function(){
+	    fc.setAttribute("style", "display:none");
+	    document.querySelector("#menu_footer:hover").setAttribute("style", "height:15%;")
+		});
+    document.querySelector(".bloc_footer").addEventListener("mouseleave", function(){
+            fc.setAttribute("style", "visibility:visible");
+            document.querySelector("#menu_footer:hover").setAttribute("style", "height:10%;")
+                });
+}
