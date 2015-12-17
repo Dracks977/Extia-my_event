1043cd8c785122f099d113e2b11a13542d82cba1
root <root@localhost.localdomain>
2015-12-17 12:07:35 -0500

    ewt

--- reunification/page/connexion.php
+++ reunification/page/connexion.php
@@ -19,7 +19,7 @@
 	        <label for='pass'>Mot de passe * : </label><input id='pass' name='pass' type='password'></input></br>
 		    </br><input type="submit" value="Je me connecte" class="bouton" /></br>
 </form>
-    <h5><i>* Champs obligatoires</i></h5><br><br><br>
+    <h5><i>* Champs obligatoires</i></h5><br>
     <span id="erreur"></span></br>
     <a href="inscription.php">Tu n'es pas encore inscrit ?</a></br>
     <a href="../Admin/">Panel admin</a></br>
@@ -33,7 +33,7 @@
     <img id="flecheorange" class="img2" src="../img/fc.svg" alt="flecheOrange"/>
     <a href="https://www.facebook.com/talentextia"><img id="facebook" class="img1" src="../img/facebook.png" alt="facebook"/></a>
     <a href="https://twitter.com/Extia_conseil"><img id="twitter" class="img1" src="../img/twitter.png" alt="twitter"/></a>
-    <a href="https://www.youtube.com/user/TalentsExtia"><img id="youtube" class="img1" src="../img/youtube.png" alt="youtube"/></a>
+    <a href="https://www.youtube.com/user/TalentsExtia"s><img id="youtube" class="img1" src="../img/youtube.png" alt="youtube"/></a>
           <a href="https://www.linkedin.com/company/extia"><img id="in" class="img1" src="../img/in.jpg" alt="linkedin"/></a>
           <a href="http://fr.viadeo.com/fr/company/extia"><img id="viadeo" class="img1" src="../img/viadeo.png" alt="viadeo"/></a>
           <img id="greatplace" class="img" src="../img/Gp2.png" alt="logoGreatPlace"/>
